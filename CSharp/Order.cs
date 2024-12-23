5065dbb68874095954289bb0029db4ee6393da50
knarknarknar <robert.j.knar@gmail.com>
2024-12-23 15:18:02 -0600

    Update Order.cs

--- CSharp/Order.cs
+++ CSharp/Order.cs
@@ -3,7 +3,7 @@ namespace OrderApi.Models
 {
     public class Order
     {
-        public int Id { get; set; } // Typically auto-generated
+        public int Id { get; set; }
         public string CustomerName { get; set; }
         public string Product { get; set; }
         public int Quantity { get; set; }
